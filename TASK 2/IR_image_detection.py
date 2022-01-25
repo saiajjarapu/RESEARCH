920e221bcad3a6635a30fe4f54d7e7ddd596fe80
saiajjarapu <matrex749@gmail.com>
2022-01-24 21:29:53 -0700

    TASK 2 progress made. Need to adjust the Canny edge detection to get rid of unwanted artifacts

--- TASK 2/IR_image_detection.py
+++ TASK 2/IR_image_detection.py
@@ -1,1 +1,35 @@
 import cv2
+from draw_contours import getContours
+import numpy as np
+import matplotlib.pyplot as plt
+
+src_img = cv2.imread("TASK 2/Science_irhand.jpg")
+imgContour = src_img.copy()
+custom_kernel = np.array([[0, -1, 0], 
+                          [-1, 5, -1],
+                          [0, -1, 0]])
+# imgblur = cv2.bilateralFilter(src_img, 9, 75, 75)
+imgblur = cv2.filter2D(src_img, -1, custom_kernel)
+imghsv = cv2.cvtColor(src_img, cv2.COLOR_BGR2HSV)
+imgGray = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
+
+heat_min = np.array([5, 50, 50], np.uint8)
+heat_max = np.array([30, 255, 255], np.uint8)
+orange_filter = cv2.inRange(imghsv, heat_min, heat_max)
+
+kernel = np.ones((5,5))
+imgDilate = cv2.dilate(orange_filter, kernel=kernel)
+
+imgCanny = cv2.Canny(imgblur, 100, 350, apertureSize=3)
+edges = cv2.bitwise_and(imgDilate, imgCanny)
+
+
+cv2.imshow("ORIGINAL", src_img)
+cv2.imshow("BLURRED", imgblur)
+cv2.imshow("GRAY SCALE", imgGray)
+cv2.imshow("DILATED", imgDilate)
+cv2.imshow("CANNY", imgCanny)
+cv2.imshow("EDGES", edges)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
---
+++ TASK 2/draw_contours.py
@@ -0,0 +1,14 @@
+import cv2
+
+def getContours(img, imgContour):
+    contours, heirachy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 900:
+            cv2.drawContours(imgContour, c, -1, (255, 0 , 255), 2)
+            p = cv2.arcLength(c, True)
+            appr = cv2.approxPolyDP(c, 0.02 * p, True)
+            x, y, w, h = cv2.boundingRect(appr)
+            cv2.rectangle(imgContour, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(imgContour, f"AREA: {int(area)} px", (x-5, y-2), cv2.FONT_HERSHEY_COMPLEX, 0.4, (255,0,0), 1)
